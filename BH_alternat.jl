4503019b7496df26dd5991bbf49091464723220e
Roger Melko <rgmelko@Dolomite.local>
2015-09-12 14:52:02 -0400

    basis generation works

--- BH_alternat.jl
+++ BH_alternat.jl
@@ -2,9 +2,9 @@
 # H = -J \sum_<ij> (b+ib-j + b-ib+j) + U/2 \sum_i n_i(n_i-1)
 
 #Number of particles
-N = 4 
+N = 3 
 #Number of sites
-M = 4 
+M = 3 
 
 #Hamiltonian parameters
 J = -1.0
@@ -12,17 +12,21 @@ U = 1.0
 
 #Hilbert Space Dimension
 D = binomial(N+M-1,M-1)
-
-println(D)
+#println(D)
 
 #start populating the basis vector
-starter = zeros(Int64,M)
-starter[1] = N
+basis = zeros(Int64,M)
+basis[1] = N
 
-v = starter
+v = copy(basis)
 println(v)
 
-for i = 2:D 
+i = 1
+exit = false
+while exit == false   #for i = 2:D 
+	i += 1            #Hilbert space counter
+	#println("count ",i)
+
 	min = 0
 	j = 1
 	while min == 0
@@ -31,27 +35,28 @@ for i = 2:D
 		end
 		j += 1
 	end
-	println("min ",min)
+	#println("min ",min)
 
-
-	if min == 1
-		v[1] -= 1
-		v[2] += 1
-	elseif min == M
-		println("end ")
+	if min == M
+		i -= 1
+		exit = true
 	else
-		v[1] = v[min]-1
-		v[min] = 0
-		v[min+1] += 1
+		if min == 1
+			v[1] -= 1
+			v[2] += 1
+		else
+			v[1] = v[min]-1
+			v[min] = 0
+			v[min+1] += 1
+		end
+		append!(basis,v)
+		println(v)
 	end
-
-
-	println(v)
-
 
 end
 
-#append!(highest,v)
+if D != i
+	println("Basis dimension error")
+end
 
-#println(highest)
-#println(v)
+println(basis)
