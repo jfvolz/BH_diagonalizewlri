fec94eb2f5296b731f178602213a78cb3466d883
Roger Melko <rgmelko@v1020-wn-88-230.campus-dynamic.uwaterloo.ca>
2015-09-15 14:00:34 -0400

    Added sparse matrix Hamiltonian
    energies match previous commit (full Hamiltonian) to 13th decimal place

--- BH_fulldiag.jl
+++ BH_fulldiag.jl
@@ -13,13 +13,13 @@ f = open("energy.dat","w")
 for U=1.0:100.0
 	
 	#Create the Hamiltonian
-	reload("BH_Hamiltonian.jl")
+	reload("BH_sparseHam.jl")
 	
 	#http://docs.julialang.org/en/release-0.3/stdlib/linalg/?highlight=lanczos
-	#d = eigs(FullHam)  
-	d = eigvals(FullHam)
+	d = eigs(SparseHam, nev=1, which=:SR) 
+	#d = eigvals(FullHam)
 	#println(U," ",d[1])
-	write(f,join((U,d[1])," "), "\n")
+	write(f,join((U,d[1][1])," "), "\n")
 	flush(f)
 
 end
