a4167a95e25e63d7a19227ee591b7eb427bd6988
Roger Melko <rgmelko@Dolomite.local>
2015-09-27 22:24:50 -0500

    RDM eigenvales sum to 1

--- BH_diagTest.jl
+++ BH_diagTest.jl
@@ -29,35 +29,73 @@ println(U," ",d[2])
 
 #Begin calculation of the reduced density matrix
 Asize = 2 # how many sites in region A
+Bsize = M - Asize
 Abasis = Int64[] 
+Bbasis = Int64[] 
 for i=0:N-1
-	tempbasis = CreateBasis(N-i,Asize)
-	append!(Abasis,tempbasis)
+	tempAbasis = CreateBasis(N-i,Asize)
+	tempBbasis = CreateBasis(N-i,Bsize)
+	append!(Abasis,tempAbasis)
+	append!(Bbasis,tempBbasis)
 end
-tempbasis = zeros(Asize)
-append!(Abasis,tempbasis) #this is the reduced basis for region A
-#println(Abasis)
+tempAbasis = zeros(Asize)
+tempBbasis = zeros(Bsize)
+append!(Abasis,tempAbasis) #this is the reduced basis for region A
+append!(Bbasis,tempBbasis) #this is the reduced basis for region B
+println(Abasis)
+println(Bbasis)
 
 #dimension of the total Hilbert space
 D = div(length(basis),M)
+println("D ",D)
 #dimension of the subspace A
 DimA = div(length(Abasis),Asize)
+DimB = div(length(Bbasis),Bsize)
 println("DimA ",DimA)
-# BDensityM = zeros(D-DimA,D-DimA) #The density matrix over B
+println("DimB ",DimB)
+
+BDensityM = zeros(DimB,DimB) #The density matrix over B
  
 #perform the Trace_A
 for j=1:DimA
-	braA = sub(Abasis,(j-1)*Asize+1:(j-1)*Asize+Asize) #unpack the bra/ket 
-	#index = SerialNum(N,Asize,braA) 
-	index = subSerialNum(sum(braA),Asize,N,braA)
-	println(j," ",index," ",braA," ",sum(braA))
+	braA = sub(Abasis,(j-1)*Asize+1:(j-1)*Asize+Asize)	#unpack the bra/ket 
+	#index = subSerialNum(sum(braA),Asize,N,braA)		#this is the position in the basis
+	#println(j," ",index," ",braA," ",sum(braA))
 
-# 	for i=1:D
-# 		bra = sub(basis,(i-1)*M+1:(i-1)*M+M) #unpack the bra/ket from the total basis vector
-# 		subbra = sub(bra,1:Asize)
-# 		println("trace ",braA," ",subbra)
-# 		println(bra," ",d[2][i]) #this is the eigenvector 
-# 
-#	end
+ 	for row=1:D
+ 		bra = sub(basis,(row-1)*M+1:(row-1)*M+M) #unpack the bra/ket from the total basis vector
+ 		subbra = sub(bra,1:Asize)
+ 		#println("trace1 ",braA," ",subbra)
+ 		#println(bra," ",d[2][row]) #this is the eigenvector 
+
+		for col=1:D
+ 			ket= sub(basis,(col-1)*M+1:(col-1)*M+M) #unpack the bra/ket from the total basis vector 
+			subket = sub(ket,1:Asize)
+			#println("trace2 ",braA," ",subket)
+
+			if subbra == braA && subket == braA #orthogonality is satisfied for the trace 
+				Bbra = sub(bra,Asize+1:M)
+				Bket = sub(ket,Asize+1:M)
+				println("Bbra ",braA," ",Bbra," ",Bket)
+				indexRow = 	subSerialNum(sum(Bbra),Bsize,N,Bbra)
+				indexCol = 	subSerialNum(sum(Bket),Bsize,N,Bket)
+				BDensityM[indexRow,indexCol] += d[2][row]*d[2][col]
+			end
+
+		end #col
+ 
+	end #row
+
+end #trace
+
+println(BDensityM)
+lambda = eig(BDensityM)
+println(lambda[1])
+println(size(lambda[1]))
+
+Lsum=0.0
+for i=1:DimB
+	Lsum += lambda[1][i]
 end
+println(Lsum)
 
