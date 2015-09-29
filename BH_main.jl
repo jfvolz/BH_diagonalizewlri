933bdf1c2982903d38cfbcb3b23c2e209f91658d
Roger Melko <rgmelko@Dolomite.local>
2015-09-29 15:50:01 -0500

    SVD used for spatial EE
    Massive speed up

--- BH_main.jl
+++ BH_main.jl
@@ -3,15 +3,16 @@
 
 
 #Number of particles
-const N = 4 
+const N = 7 
 #Number of sites
-const M = 4 
+const M = 7 
 #size of region A
 const Asize = 2
 
 include("BH_basis.jl")
 include("BH_sparseHam.jl")
 include("spatialEntropy.jl")
+include("spatialEntropy_SVD.jl")
 
 basis = CreateBasis(N,M)
 
@@ -19,19 +20,23 @@ basis = CreateBasis(N,M)
 T = -1.0
 
 f = open("output.dat","w")
-for U=1.0:100.0
+#for U=1.0:100.0
+U = 1.0
 	
 	#Create the Hamiltonian
 	SparseHam = CreateSparseHam(basis,T,U)
 
 	#http://docs.julialang.org/en/release-0.3/stdlib/linalg/?highlight=lanczos
 	d = eigs(SparseHam, nev=1, which=:SR) 
-	#d = eigvals(FullHam)
-	#println(U," ",d[1])
-	s2 = SpatialEE(N,M,Asize,d)
+
+    #Calcualte the second Renyi entropy
+	@time s2 = SpatialEE(N,M,Asize,d)
+	@time s2b = SpatialEE_SVD(N,M,Asize,d)
+	println(s2," ",s2b)
+
 	write(f,join((U,d[1][1],s2)," "), "\n")
 	flush(f)
 
-end
+#end
 close(f)
 
