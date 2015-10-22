ccc5c2386975f26fb4868c8442bfd7b9824d5bf2
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2015-10-22 09:22:57 +0200

    Use do block for open

--- BH_main.jl
+++ BH_main.jl
@@ -19,24 +19,21 @@ basis = CreateBasis(N,M)
 #Hamiltonian parameters
 T = -1.0
 
-f = open("output.dat","w")
-for U=1.0:0.5:20.0
-	
-	#Create the Hamiltonian
-	SparseHam = CreateSparseHam(basis,T,U)
+open("output.dat","w") do f
+	for U=1.0:0.5:20.0
+		#Create the Hamiltonian
+		SparseHam = CreateSparseHam(basis,T,U)
 
-	#Perform the Lanczos diagonalization to obtain the lowest eigenvector
-	#http://docs.julialang.org/en/release-0.3/stdlib/linalg/?highlight=lanczos
-	d = eigs(SparseHam, nev=1, which=:SR) 
+		#Perform the Lanczos diagonalization to obtain the lowest eigenvector
+		#http://docs.julialang.org/en/release-0.3/stdlib/linalg/?highlight=lanczos
+		d = eigs(SparseHam, nev=1, which=:SR)
 
-    #Calcualte the second Renyi entropy
-	s2_particle = ParticleEE_SVD(N, M, Asize, d[2])
-	s2_spatial = SpatialEE_SVD(N,M,Asize,d)
-	#println(s2," ",s2b)
+		#Calcualte the second Renyi entropy
+		s2_particle = ParticleEE_SVD(N, M, Asize, d[2])
+		s2_spatial = SpatialEE_SVD(N,M,Asize,d)
+		#println(s2," ",s2b)
 
-	write(f,join((U,d[1][1],s2_particle,s2_spatial)," "), "\n")
-	flush(f)
-
+		write(f,join((U,d[1][1],s2_particle,s2_spatial)," "), "\n")
+		flush(f)
+	end
 end
-close(f)
-
