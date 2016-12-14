cc1f90e95e381503e1c248a3d341c9d6e519b8a1
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2016-12-14 17:11:14 -0500

    Optimize particle entanglement entropy
    Using the occupation basis instead of the first-quantized particle basis
    results in much smaller matrices and faster execution. The particle
    entropy calculation is still the bottleneck, but by a much smaller
    factor. For example, with 8 sites and 8 particles, the overall
    calculation (including diagonalization and spatial entropy) is over 100
    times faster.

--- src/particle_entropy.jl
+++ src/particle_entropy.jl
@@ -2,45 +2,25 @@
 Calculate the particle entanglement entropy for a subset A, using the SVD.
 """
 function particle_entropy(basis::AbstractSzbasis, Asize::Int, d::Vector{Float64})
-    M = basis.K
-    Bsize = basis.N - Asize
-    # Dimensions of partition Hilbert spaces
-    DimA = M^Asize
-    DimB = M^Bsize
-
+    basisA, basisB = particle_entropy_bases(basis, Asize)
     # Matrix to SVD
-    Amatrix = zeros(Float64, DimA, DimB)
+    Amatrix = zeros(Float64, length(basisA), length(basisB))
 
-    fN = factorial(basis.N)
-    occupA = Array(Int, M)
-    occup = Array(Int, M)
+    for (i, braA) in enumerate(basisA)
+        for (j, braB) in enumerate(basisB)
+            bra = braA + braB
+            bra in basis || continue
 
-    for i=1:DimA
-        fill!(occupA, 0)
-
-        for k=1:Asize
-            occupA[1 + div(i - 1, M^(k - 1)) % M] += 1
-        end
-
-        for j=1:DimB
-            copy!(occup, occupA)
-
-            for k=1:Bsize
-                occup[1 + div(j - 1, M^(k - 1)) % M] += 1
+            norm = 1.0
+            for k in 1:basis.K
+                norm *= binomial(bra[k], braA[k])
             end
 
-            if occup in basis
-                norm = 1 / fN
-                for x in occup
-                    norm *= factorial(x)
-                end
-
-                Amatrix[i, j] = sqrt(norm) * d[serial_num(basis, occup)]
-            end
+            Amatrix[i, j] = sqrt(norm) * d[serial_num(basis, bra)]
         end
     end
 
-    S = svdvals(Amatrix)
+    S = svdvals(Amatrix) / sqrt(binomial(basis.N, basisA.N))
     err = abs(sum(S.^2) - 1.0)
 
     if err > 1e-12
@@ -48,4 +28,32 @@ function particle_entropy(basis::AbstractSzbasis, Asize::Int, d::Vector{Float64}
     end
 
     -log(sum(S.^4))
+end
+
+
+"""
+Generate the bases used for the particle entanglement entropy.
+"""
+function particle_entropy_bases(basis::Szbasis, Asize::Int)
+    Bsize = basis.N - Asize
+    basisA = Szbasis(basis.K, Asize)
+    basisB = Szbasis(basis.K, Bsize)
+
+    basisA, basisB
+end
+
+function particle_entropy_bases(basis::RestrictedSzbasis, Asize::Int)
+    Bsize = basis.N - Asize
+    if Asize <= basis.M
+        basisA = Szbasis(basis.K, Asize)
+    else
+        basisA = RestrictedSzbasis(basis.K, Asize, basis.M)
+    end
+    if Bsize <= basis.M
+        basisB = Szbasis(basis.K, Bsize)
+    else
+        basisB = RestrictedSzbasis(basis.K, Bsize, basis.M)
+    end
+
+    basisA, basisB
 end
